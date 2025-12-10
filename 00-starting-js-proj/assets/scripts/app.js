1c1e258434fba373324f8d34504f096be22415fc
David Regaldi <david.regaldi@gmail.com>
2025-12-10 13:05:05 +0100

    feat: introduce local user message variables and disable module imports and exports

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,7 +1,14 @@
-//import { apikey } from "./util.js";
+// import { apikey } from "./util.js";
 // import apikey from "./util.js";
-import { apikey, abc as content } from "./util.js";
+// import { apikey, abc as content } from "./util.js";
 // import * as util from "./util.js";
-console.log(apikey);
+
+// console.log(apikey);
 // console.log(util.apikey, util.abc, util.default);
-console.log(content);
+// console.log(content);
+
+let userMessage = "Hello World !";
+const userMessage2 = "Hello World !!";
+// console.log("hello world !");
+console.log(userMessage);
+console.log(userMessage2);
--- 00-starting-js-proj/assets/scripts/util.js
+++ 00-starting-js-proj/assets/scripts/util.js
@@ -1,4 +1,4 @@
 // export let apikey = "glkfdnjgdnlkg";
-export default "glkfdnjgdnlkg";
-export let apikey = "glkfdnjgdnlkg";
-export let abc = "abc";
+// export default "glkfdnjgdnlkg";
+// export let apikey = "glkfdnjgdnlkg";
+// export let abc = "abc";
