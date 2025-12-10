f4098ca2c5932dd69717124dfff125efbbb831e9
David Regaldi <david.regaldi@gmail.com>
2025-12-10 13:17:03 +0100

    Replace variable logging with examples of arithmetic, string, and comparison operators, and add a conditional statement.

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -7,8 +7,20 @@
 // console.log(util.apikey, util.abc, util.default);
 // console.log(content);
 
-let userMessage = "Hello World !";
-const userMessage2 = "Hello World !!";
+// let userMessage = "Hello World !";
+// const userMessage2 = "Hello World !!";
 // console.log("hello world !");
-console.log(userMessage);
-console.log(userMessage2);
+// console.log(userMessage);
+// console.log(userMessage2);
+
+console.log(10 * 5);
+console.log("hello" + "world");
+console.log(10 === 5);
+console.log(10 === 10);
+console.log(10 >= 5);
+
+if (10 === 10) {
+    console.log("works !");
+}
+
+
