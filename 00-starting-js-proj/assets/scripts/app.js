7dc38f3bd81baa31a837c140f49273c0c7e1feaf
David Regaldi <david.regaldi@gmail.com>
2025-12-11 09:29:06 +0100

    Using Functions as Values (callback)

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -140,8 +140,22 @@
 //     console.log(hobby);
 // }
 
-const list = document.querySelector("ul");
-list.remove();
+// const list = document.querySelector("ul");
+// list.remove();
 
+function handleTimeout() {
+    console.log("Timeout!");
+}
+const handleTimeout2 = () => {
+    console.log("Timeout ... again !");
+}
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(() => { console.log("More timing out...") }, 4000)
+
+function greeter(greetFn) {
+    greetFn();
+}
+greeter(() => console.log("Hi"));
 
 
