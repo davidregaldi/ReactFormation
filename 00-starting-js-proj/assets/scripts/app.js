4783d6deff2ebdea27b9fb31ef06091663d80eba
David Regaldi <david.regaldi@gmail.com>
2025-12-11 09:32:41 +0100

    Defining Functions Inside of Functions

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -143,19 +143,27 @@
 // const list = document.querySelector("ul");
 // list.remove();
 
-function handleTimeout() {
-    console.log("Timeout!");
-}
-const handleTimeout2 = () => {
-    console.log("Timeout ... again !");
-}
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => { console.log("More timing out...") }, 4000)
+// function handleTimeout() {
+//     console.log("Timeout!");
+// }
+// const handleTimeout2 = () => {
+//     console.log("Timeout ... again !");
+// }
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => { console.log("More timing out...") }, 4000)
 
-function greeter(greetFn) {
-    greetFn();
+// function greeter(greetFn) {
+//     greetFn();
+// }
+// greeter(() => console.log("Hi"));
+
+function init() {
+    function greet() {
+        console.log("Hello");
+    }
+    greet();
 }
-greeter(() => console.log("Hi"));
+init();
 
 
