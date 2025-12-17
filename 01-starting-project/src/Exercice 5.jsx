a99a2e20b7497de523c80bc7dbfe947ec0ecd7f3
David Regaldi <david.regaldi@gmail.com>
2025-12-17 14:04:04 +0100

    Exercice 5: Working with Props.

---
+++ 01-starting-project/src/Exercice 5.jsx
@@ -0,0 +1,26 @@
+// export function CourseGoal({title, description}) {
+//   return (
+//     <li>
+//       <h2>{title}</h2>
+//       <p>{description}</p>
+//     </li>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div id="app" data-testid="app">
+//       <h1>Time to Practice</h1>
+//       <p>One course, many goals! 🎯</p>
+//       <ul>
+//         {/* OUTPUT AT LEAST TWO CourseGoal components here */}
+//         {/* One of them should have a title of “Learn React” and a description of “In-depth” */}
+//         <CourseGoal title="learn react" description="In-depth"/>
+//         <CourseGoal title="practice" description="Practice working with React, components, etc..."/>
+        
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
