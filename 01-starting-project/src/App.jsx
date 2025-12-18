b475d9b6c8b8d389f12acf6b0e2ff02031a62c91
David Regaldi <david.regaldi@gmail.com>
2025-12-18 15:51:46 +0100

    Component Composition: The special "children" Prop.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,8 @@
 
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -19,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="exemples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton label="Components" />
+            <TabButton label="JSX" />
+            <TabButton label="Props" />
+            <TabButton label="State" />
+          </menu>
         </section>
       </main>
     </div>
--- 01-starting-project/src/components/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -1,5 +1,3 @@
-import './CoreConcept.css'
-
 export default function CoreConcept({ title, description, image }) {
     return <li>
         <img src={image} alt={title} />
