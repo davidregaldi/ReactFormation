a75ac2652a1e42d448ad3707925ac0af5321aa8d
David Regaldi <david.regaldi@gmail.com>
2025-12-18 16:10:09 +0100

    Exercice 6: Component Composition

---
+++ 01-starting-project/src/exercices/Exercice6/Card.jsx
@@ -0,0 +1,9 @@
+import "./index.css"
+export default function Card({ name, children }) {
+    return (
+        <article className="card">
+            <h2>{name}</h2>
+            {children}
+        </article>
+    );
+}
