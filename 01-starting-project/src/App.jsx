31c9d1f9dc8edf6afb93e572f587c9e6be9b2e60
David Regaldi <david.regaldi@gmail.com>
2025-12-19 09:59:13 +0100

    Delivering & Outputyting Data Based on State

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,13 +1,13 @@
 
 import { useState } from "react";
 
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a tab');
+  const [selectedTopic, setSelectedTopic] = useState('components');
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
@@ -36,10 +36,18 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <article>{selectedTopic}</article>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
-    </div>
+    </div >
   );
 }
 
